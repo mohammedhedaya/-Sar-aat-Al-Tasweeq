eb85b2a26b9699e9d5d3acbb99bf826d4b707e0b
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-09-11 13:39:25 +0300

    Finish SuccessSignUpPage [2]

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -18,6 +18,22 @@ class Assets {
   /// assets/images/suadiFlag.svg
   static const String imagesSuadiFlag = "assets/images/suadiFlag.svg";
 
+  /// Assets for imagesSuccesscircle1
+  /// assets/images/successcircle1.svg
+  static const String imagesSuccesscircle1 = "assets/images/successcircle1.svg";
+
+  /// Assets for imagesSuccesscircle2
+  /// assets/images/successcircle2.svg
+  static const String imagesSuccesscircle2 = "assets/images/successcircle2.svg";
+
+  /// Assets for imagesSuccesscircle3
+  /// assets/images/successcircle3.svg
+  static const String imagesSuccesscircle3 = "assets/images/successcircle3.svg";
+
+  /// Assets for imagesSuccesscircle4
+  /// assets/images/successcircle4.svg
+  static const String imagesSuccesscircle4 = "assets/images/successcircle4.svg";
+
   /// Assets for imagesWhitecircle
   /// assets/images/whitecircle.svg
   static const String imagesWhitecircle = "assets/images/whitecircle.svg";
--- lib/features/auth/presentation/views/success_sign_up_page.dart
+++ lib/features/auth/presentation/views/success_sign_up_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shopping_chart/core/utils/app_colors.dart';
 import 'package:shopping_chart/core/utils/app_text_style.dart';
+import 'package:shopping_chart/core/utils/assets.dart';
 
 class SuccessSignUpPage extends StatelessWidget {
   const SuccessSignUpPage({super.key});
@@ -9,55 +10,83 @@ class SuccessSignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          SizedBox(height: 200.h),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 45),
-            child: Column(
-              children: [
-                const Center(
-                  child: Icon(
-                    Icons.check_circle_outline_sharp,
-                    color: AppColors.whiteColor,
-                    size: 140,
-                  ),
-                ),
-                const SizedBox(height: 35),
-                Text(
-                  "تم تسجيل حسابك بنجاح",
-                  style: AppStyles.style25W800,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 125.h),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          SvgPicture.asset(
+            Assets.imagesSuccesscircle1,
+          ),
+          Positioned(
+            top: 0,
+            left: 40,
+            child: SvgPicture.asset(
+              Assets.imagesSuccesscircle2,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            right: 50,
+            child: SvgPicture.asset(
+              Assets.imagesSuccesscircle3,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            child: SvgPicture.asset(
+              Assets.imagesSuccesscircle4,
+            ),
+          ),
+          Column(
+            children: [
+              SizedBox(height: MediaQuery.sizeOf(context).height * 0.3),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 45),
+                child: Column(
                   children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        constraints: const BoxConstraints(
-                          minHeight: 64,
-                          minWidth: 64,
-                        ),
-                        decoration: const BoxDecoration(
-                          color: AppColors.circleGray,
-                          shape: BoxShape.circle,
-                        ),
-                        child: const Icon(
-                          Icons.arrow_back_outlined,
-                          color: AppColors.whiteColor,
-                        ),
+                    const Center(
+                      child: Icon(
+                        Icons.check_circle_outline_sharp,
+                        color: AppColors.whiteColor,
+                        size: 140,
                       ),
                     ),
+                    const SizedBox(height: 35),
                     Text(
-                      "أبدأ التسويق",
-                      style: AppStyles.style32W700,
+                      "تم تسجيل حسابك بنجاح",
+                      style: AppStyles.style25W800,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: MediaQuery.sizeOf(context).height * 0.16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            constraints: const BoxConstraints(
+                              minHeight: 64,
+                              minWidth: 64,
+                            ),
+                            decoration: const BoxDecoration(
+                              color: AppColors.circleGray,
+                              shape: BoxShape.circle,
+                            ),
+                            child: const Icon(
+                              Icons.arrow_back_outlined,
+                              color: AppColors.whiteColor,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "أبدأ التسويق",
+                          style: AppStyles.style32W700,
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
