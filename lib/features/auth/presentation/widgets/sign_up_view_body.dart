cba109ae25039531cdef494aa953cbd09790574f
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-09-12 11:34:37 +0300

    Make Login View Scrollable

--- lib/features/auth/presentation/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/widgets/sign_up_view_body.dart
@@ -1,134 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shopping_chart/core/utils/app_colors.dart';
 import 'package:shopping_chart/core/utils/app_text_style.dart';
 import 'package:shopping_chart/core/utils/assets.dart';
+import 'package:shopping_chart/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:shopping_chart/features/auth/presentation/cubit/auth_state.dart';
 import 'package:shopping_chart/features/auth/presentation/widgets/auth_text_field.dart';
 
 class SignUpViewBody extends StatelessWidget {
-  const SignUpViewBody({
-    super.key,
-  });
+  const SignUpViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: [
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: SvgPicture.asset(
-                Assets.imagesWhitecircle,
-                height: 200.h,
-                fit: BoxFit.fill,
-              ),
-            ),
-            Positioned(
-              top: 30,
-              right: 24,
-              child: Text(
-                "إنشاء حساب",
-                style: AppStyles.style40W600.copyWith(
-                  color: AppColors.blackColor,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 230,
-              left: 41,
-              right: 26,
-              child: Column(
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        AuthCubit cubit = BlocProvider.of<AuthCubit>(context);
+        return SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Form(
+              key: cubit.signUpFormKey,
+              child: Stack(
                 children: [
-                  CustomAuthTextField(
-                    hintText: 'الاسم الأول',
-                    onChanged: (firstName) {},
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: SvgPicture.asset(
+                      Assets.imagesWhitecircle,
+                      height: 200.h,
+                      fit: BoxFit.fill,
+                    ),
                   ),
-                  SizedBox(height: 21.h),
-                  CustomAuthTextField(
-                    hintText: 'الاسم الثانى',
-                    onChanged: (secondName) {},
+                  Positioned(
+                    top: 30,
+                    right: 24,
+                    child: Text(
+                      "إنشاء حساب",
+                      style: AppStyles.style40W600.copyWith(
+                        color: AppColors.blackColor,
+                      ),
+                    ),
                   ),
-                  SizedBox(height: 21.h),
-                  CustomAuthTextField(
-                    hintText: 'الاسم الأخير',
-                    onChanged: (lastName) {},
+                  Positioned(
+                    top: 230,
+                    left: 41,
+                    right: 26,
+                    child: Column(
+                      children: [
+                        CustomAuthTextField(
+                          hintText: 'الاسم الأول',
+                          onChanged: (firstName) {
+                            cubit.firstName = firstName;
+                          },
+                        ),
+                        SizedBox(height: 40.h),
+                        CustomAuthTextField(
+                          hintText: 'الاسم الثانى',
+                          onChanged: (secondName) {
+                            cubit.secondName = secondName;
+                          },
+                        ),
+                        SizedBox(height: 40.h),
+                        CustomAuthTextField(
+                          hintText: 'الاسم الأخير',
+                          onChanged: (lastName) {
+                            cubit.lastName = lastName;
+                          },
+                        ),
+                        SizedBox(height: 40.h),
+                        CustomAuthTextField(
+                          hintText: 'الأيميل',
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (email) {
+                            if (email == null || email.isEmpty) {
+                              return 'من فضلك قم بادخال بريد الكتروني';
+                            } else if (!RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+                            ).hasMatch(email)) {
+                              return 'من فضلك قم بادخال بريد الكتروني صحيح';
+                            }
+                            return null;
+                          },
+                          onChanged: (email) {
+                            cubit.email = email;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 21.h),
-                  CustomAuthTextField(
-                    hintText: 'الأيميل',
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (email) {
-                      if (email == null || email.isEmpty) {
-                        return 'من فضلك قم بادخال بريد الكتروني';
-                      } else if (!RegExp(
-                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
-                      ).hasMatch(email)) {
-                        return 'من فضلك قم بادخال بريد الكتروني صحيح';
-                      }
-                      return null;
-                    },
-                    onChanged: (email) {},
-                  ),
-                  SizedBox(height: 7.h),
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: -50,
-              child: Container(
-                height: 200.h,
-                width: 200.w,
-                decoration: const BoxDecoration(
-                  color: AppColors.whiteColor,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(300),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 110.h,
-              left: 41.w,
-              child: Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      context.push('/signUpProfileView');
-                    },
+                  Positioned(
+                    bottom: 0,
+                    left: -50.w,
                     child: Container(
-                      constraints: BoxConstraints(
-                        minHeight: 64.h,
-                        minWidth: 64.w,
-                      ),
+                      height: 170.h,
+                      width: 170.w,
                       decoration: const BoxDecoration(
-                        color: AppColors.loginBtn,
-                        shape: BoxShape.circle,
-                      ),
-                      child: Center(
-                        child: SvgPicture.asset(
-                          Assets.imagesArrowForwardLoginPage,
+                        color: AppColors.whiteColor,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(300),
                         ),
                       ),
                     ),
                   ),
-                  SizedBox(width: MediaQuery.of(context).size.width * 0.2),
-                  Text(
-                    "تسجيل\nالحساب",
-                    style: AppStyles.style32W700,
+                  Positioned(
+                    bottom: 65.h,
+                    left: 41.w,
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            if (cubit.signUpFormKey.currentState!.validate()) {
+                              context.push('/signUpProfileView');
+                            }
+                          },
+                          child: Container(
+                            constraints: BoxConstraints(
+                              minHeight: 64.h,
+                              minWidth: 64.w,
+                            ),
+                            decoration: const BoxDecoration(
+                              color: AppColors.loginBtn,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Center(
+                              child: SvgPicture.asset(
+                                Assets.imagesArrowForwardLoginPage,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.2),
+                        Text(
+                          "تسجيل\nالحساب",
+                          style: AppStyles.style32W700,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
