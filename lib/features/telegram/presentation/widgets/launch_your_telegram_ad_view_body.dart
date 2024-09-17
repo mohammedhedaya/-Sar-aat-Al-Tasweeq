6a069d46441237c6dc38f803c172736381c6057a
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-09-17 12:53:50 +0300

    Finish Telegram Delete Page View

--- lib/features/telegram/presentation/widgets/launch_your_telegram_ad_view_body.dart
+++ lib/features/telegram/presentation/widgets/launch_your_telegram_ad_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:shopping_chart/features/telegram/presentation/views/launch_pages_view/telegram_delete_page_view.dart';
 import 'package:shopping_chart/features/telegram/presentation/views/launch_pages_view/telegram_page_view.dart';
 import 'package:shopping_chart/features/telegram/presentation/views/launch_pages_view/telegram_phone_page_view.dart';
 import '../../../../core/utils/app_colors.dart';
@@ -147,21 +148,6 @@ class _LaunchYourTelegramAdViewBodyState
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-
-
-class TelegramDeletePageView extends StatelessWidget {
-  const TelegramDeletePageView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Deleted Page",
       ),
     );
   }
