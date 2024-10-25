a9c1872d5b90e73c30f717ed00cd85c7a890758c
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-10-25 22:01:58 +0300

    Launch Your Insta Ad View Light Mode

--- lib/features/instagram/presentation/widgets/launch_your_insta_ad_view_body.dart
+++ lib/features/instagram/presentation/widgets/launch_your_insta_ad_view_body.dart
@@ -69,7 +69,9 @@ class _LaunchYourInstaAdViewBodyState extends State<LaunchYourInstaAdViewBody> {
                 height: 268.h,
                 padding: const EdgeInsets.all(20),
                 decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.10),
+                  color: isDark
+                      ? Colors.white.withOpacity(0.10)
+                      : AppColors.fillLight,
                   borderRadius: BorderRadius.circular(20.r),
                   border: Border.all(
                     color: isDark ? Colors.transparent : AppColors.blueLight,
@@ -81,7 +83,7 @@ class _LaunchYourInstaAdViewBodyState extends State<LaunchYourInstaAdViewBody> {
                           ? SvgPicture.asset(
                               Assets.imagesUploadIcon,
                               colorFilter: ColorFilter.mode(
-                                isDark ? Colors.white : AppColors.blueLight,
+                                isDark ? Colors.white : AppColors.yellowLight,
                                 BlendMode.srcIn,
                               ),
                             )
@@ -92,7 +94,7 @@ class _LaunchYourInstaAdViewBodyState extends State<LaunchYourInstaAdViewBody> {
                       : SvgPicture.asset(
                           Assets.imagesUploadIcon,
                           colorFilter: ColorFilter.mode(
-                            isDark ? Colors.white : AppColors.blueLight,
+                            isDark ? Colors.white : AppColors.yellowLight,
                             BlendMode.srcIn,
                           ),
                         ),
@@ -103,15 +105,17 @@ class _LaunchYourInstaAdViewBodyState extends State<LaunchYourInstaAdViewBody> {
             CustomAuthTextField(
               onChanged: (value) {},
               hintText: "Writeacomment".tr(context: context),
-              fiiledColor: Colors.white.withOpacity(0.10),
+              fiiledColor:
+                  isDark ? Colors.white.withOpacity(0.10) : AppColors.fillLight,
               hintStyle: AppStyles.style16W600.copyWith(
-                color: const Color(0xffB0B0B0),
+                color: isDark ? const Color(0xffB0B0B0) : Colors.black54,
               ),
               maxLines: 3,
             ),
             SizedBox(height: 21.h),
             CustomAuthTextField(
-              fiiledColor: AppColors.whiteColor.withOpacity(0.10),
+              fiiledColor:
+                  isDark ? Colors.white.withOpacity(0.10) : AppColors.fillLight,
               hintText: "addLink".tr(context: context),
               hintStyle: AppStyles.style12W700.copyWith(
                 color:
@@ -119,15 +123,20 @@ class _LaunchYourInstaAdViewBodyState extends State<LaunchYourInstaAdViewBody> {
               ),
               suffixIcon: Transform.scale(
                 scale: 0.5,
-                child: SvgPicture.asset(
-                  Assets.imagesLinkTeal,
-                ),
+                child: isDark
+                    ? SvgPicture.asset(
+                        Assets.imagesLinkTeal,
+                      )
+                    : SvgPicture.asset(
+                        Assets.imagesLinkLight,
+                      ),
               ),
               onChanged: (value) {},
             ),
             SizedBox(height: 21.h),
             CustomAuthTextField(
-              fiiledColor: AppColors.whiteColor.withOpacity(0.10),
+              fiiledColor:
+                  isDark ? Colors.white.withOpacity(0.10) : AppColors.fillLight,
               hintText: "addLocation".tr(context: context),
               hintStyle: AppStyles.style12W700.copyWith(
                 color:
@@ -135,9 +144,13 @@ class _LaunchYourInstaAdViewBodyState extends State<LaunchYourInstaAdViewBody> {
               ),
               suffixIcon: Transform.scale(
                 scale: 0.5,
-                child: SvgPicture.asset(
-                  Assets.imagesLocationTeal,
-                ),
+                child: isDark
+                    ? SvgPicture.asset(
+                        Assets.imagesLocationTeal,
+                      )
+                    : SvgPicture.asset(
+                        Assets.imagesLocationLight,
+                      ),
               ),
               onChanged: (value) {},
             ),
@@ -166,7 +179,6 @@ class _LaunchYourInstaAdViewBodyState extends State<LaunchYourInstaAdViewBody> {
                       EdgeInsets.symmetric(horizontal: 22.w, vertical: 4.h),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(5.r),
-                    color: isDark ? null : Colors.orange,
                     gradient: isDark
                         ? const LinearGradient(
                             colors: [
@@ -176,7 +188,14 @@ class _LaunchYourInstaAdViewBodyState extends State<LaunchYourInstaAdViewBody> {
                             begin: Alignment.centerLeft,
                             end: Alignment.centerRight,
                           )
-                        : null,
+                        : const LinearGradient(
+                            colors: [
+                              AppColors.linearLight1,
+                              AppColors.linearLight2,
+                            ],
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                          ),
                   ),
                   child: Center(
                     child: Text(
